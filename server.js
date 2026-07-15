ffaab45273bff5f7fe543e8db97d4c4fdded13a7
qasimmasimm <h.qasimasim@gmail.com>
2026-07-15 19:28:14 +0500

    -veercel updates in pkg.json and vercel.json

--- server.js
+++ server.js
@@ -1,47 +1,38 @@
-const express=require("express");
+const express = require("express");
 require("dotenv").config();
-const {connect}=require("mongoose");
-const path=require("path");
-const morgan=require("morgan");
-const cors=require("cors")
-
+const { connect } = require("mongoose");
+const path = require("path");
+const morgan = require("morgan");
+const cors = require("cors");
 
 //my routerss
-const projectRouter=require("./routes/project.route");
-const userRouter=require("./routes/user.route");
+const projectRouter = require("./routes/project.route");
+const userRouter = require("./routes/user.route");
 const contactRouter = require("./routes/contact.route");
-
-
-
 
 const app = express();
 
-
-
-app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    "https://spline-portfolio-frontend.vercel.app"
-  ],
-  credentials: true
-}));
-
-
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://spline-portfolio-frontend.vercel.app",
+    ],
+    credentials: true,
+  }),
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
-app.use("/upload",express.static("upload"))
+app.use("/upload", express.static("upload"));
 
-
-app.use("/auth",userRouter);
+app.use("/auth", userRouter);
 app.use("/contact", contactRouter);
-app.use("/projects",projectRouter);
+app.use("/projects", projectRouter);
 
+connect(process.env.MONGODB_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
 
-
-
-app.listen(8080, () => {
-    connect(process.env.MONGODB_URI);
-    console.log("app is running at vercel");
-})
+module.exports = app;
