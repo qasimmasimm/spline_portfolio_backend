df43208e0dd2aef07d4377e36f0bd663e6124d86
qasimmasimm <h.qasimasim@gmail.com>
2026-07-15 18:13:37 +0500

    created the mongodb uri

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const app = express();
 
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://spline-portfolio-frontend-qjmc-n30tun6en-qasimmasimms-projects.vercel.app",
   credentials: true
 }));
 app.use(express.json());
