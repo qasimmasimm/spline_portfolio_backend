c41358c28f89b29d434a512f4fd6e30cb0a22f96
qasimmasimm <h.qasimasim@gmail.com>
2026-07-16 18:55:32 +0500

    add mongoose in server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 require("dotenv").config();
 const { connect } = require("mongoose");
+const mongoose=require("mongoose")
 const path = require("path");
 const morgan = require("morgan");
 const cors = require("cors");
